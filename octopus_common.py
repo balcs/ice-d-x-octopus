ed8384e7ea4f55a2f34f873a9e2de67660aecb9d
Greg Balco <balcs@bgc.org>
2021-08-03 15:36:42 -0700

    remove software developer from hiring

--- octopus_common.py
+++ octopus_common.py
@@ -39,7 +39,7 @@ def begin_page(name_string):
 	# section adding support page
 	str = str + '<center><table width=1000>'
 	str = str + '<tr><td><hr></td></tr>'
-	str = str + '<tr><td bgcolor=#D0D0FF class=standard>The ICE-D project is now hiring a <b>postdoctoral researcher<b> and a <b>software developer</b>. Click'
+	str = str + '<tr><td bgcolor=#D0D0FF class=standard>The ICE-D project is now hiring a <b>postdoctoral researcher</b>. Click'
 	str = str + '<a href="http://www.ice-d.org/hiring">'
 	str = str + ' here </a> for information. </td></tr>'
 	str = str + '<tr><td><hr></td></tr><table></center>'
